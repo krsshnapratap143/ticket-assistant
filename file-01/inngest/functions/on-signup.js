dba3437a20a095e35c607fef1cb80c6fc4f195fe
krsshna <krsshnayadav@gmail.com>
2025-08-15 12:44:08 +0530

    added inngest and utils

---
+++ file-01/inngest/functions/on-signup.js
@@ -0,0 +1,6 @@
+// import { inngest } from "../client";
+
+// export const onUserSignup = inngest.createFunction(
+//     {id: "on-user-signup", retries: 2},
+//     {event: "user/signup"},
+// );
---
+++ file-01/utils/mailer.js
@@ -0,0 +1,28 @@
+import { err } from "inngest/types";
+import nodemailer from "nodemailer"
+
+export const sendMail = async (to, subject, text) => {
+    try {
+        const transporter = nodemailer.createTransport({
+        host: process.env.MAILTRAP_SMTP_HOST,
+        port: process.env.MAILTRAP_SMTP_PORT,
+        secure: false, // true for 465, false for other ports
+        auth: {
+        user: process.env.MAILTRAP_SMTP_USER,
+        pass: process.env.MAILTRAP_SMTP_PASS,
+        },
+        });
+        const info = await transporter.sendMail({
+            from: "Inngest TMS",
+            to,
+            subject,
+            text
+        });
+        console.log("message sent : ", info.messageId);
+        return info
+    }
+    catch (error) {
+        console.log("mail error", error.message)
+        throw error
+    }
+};
