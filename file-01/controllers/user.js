dda4972e686f804dfb04cc0312644c3bcb11215a
krsshna <krsshnayadav@gmail.com>
2025-08-25 15:23:03 +0530

    updated on-signup.js

---
+++ file-01/controllers/user.js
@@ -0,0 +1,32 @@
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import User from "../models/user.js"
+import { inngest } from "../inngest/client.js"
+
+export const signup = async (req, res) => {
+    const {email, password, skills =[]} = req.body
+    try {
+        const hashed = bcrypt.hash(password, 10)
+        const user = await User.create({email, password: hashed, skills})
+        // fire inngest event 
+        await inngest.send({
+            name: "user/signup", 
+            data: {
+                email 
+            }
+        });
+        const token = jwt.sign(
+            {_id: user._id, role: user.role}, 
+            process.env.JWT_SECRET
+        );
+        res.json({user, token})
+    } catch (error) {
+        res.status(500).json({
+            error: "signup failed", details: error.message
+        })
+    }
+}; 
+
+export const login = async (req, res) => {
+    const {email, password} = req.body
+}
