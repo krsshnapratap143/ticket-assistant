6dcb1288791fd2bb28596ed82cdb4595327e4914
krsshna <krsshnayadav@gmail.com>
2025-08-28 15:20:41 +0530

    complete authentication with inngest workers

--- file-01/routes/user.js
+++ file-01/routes/user.js
@@ -1,5 +1,15 @@
+import { getUsers, login, logout, signup, updateUser } from "../controllers/user.js"
 import express from "express"
 
+import { authenticate } from "../middlewares/auth.js"
+
 const router = express.Router()
+
+router.post("/update-user", authenticate, updateUser)
+router.get("/users", authenticate, getUsers)
+
+router.post("/signup", signup)
+router.post("/login", login)
+router.post("/logout", logout)
 
 export default router
